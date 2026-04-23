9ac79a9c953612f2e53b5eb8dbecdea76afbbc8d
Dennis Upton <108183460+dennisupton@users.noreply.github.com>
2026-04-23 09:59:35 +0100

    switch to oop approach

---
+++ inspector.py
@@ -0,0 +1,48 @@
+
+def hasEnd(node):
+    if node.startswith("!DOCTYPE") or node.startswith("html") or node.startswith("meta"):
+        return False
+    return True
+
+class inspectorPanel:
+    def __init__(self):
+        self.nodes = []
+        self.selectedNode = 0
+
+    def up(self):
+        self.selectedNode -= 1
+        if self.selectedNode < 0:
+            self.selectedNode = 0
+        
+    
+    def down(self):
+        self.selectedNode += 1
+        if self.selectedNode > len(self.nodes)-1:
+            self.selectedNode = len(self.nodes)-1
+
+    def parse(self,html):
+        self.nodes = []
+        indent = 0
+        newNodeName = False
+        nodeName = ""
+        for i in html:
+            if i == "<":
+                newNodeName = True
+            elif newNodeName and i == "/":
+                newNodeName = False
+                indent -= 1
+            elif i =="/":
+                indent -= 1
+            elif newNodeName and i == ">":
+                self.nodes.append(node(nodeName, indent))
+                if hasEnd(nodeName):
+                    indent += 1
+                nodeName = ""
+                newNodeName = False
+            elif newNodeName:
+                nodeName += i
+    
+class node:
+    def __init__(self, name, indent):
+        self.name = name
+        self.indent = indent
--- main.py
+++ main.py
@@ -1,56 +1,45 @@
 from blessed import Terminal
 import sys
 import file
+import inspector
 term = Terminal()
+
 
 
 def render(text):
     for i in text[0:len(text)-10]:
         print(i)
 
-def hasEnd(node):
-    if node.startswith("!DOCTYPE") or node.startswith("html") or node.startswith("meta"):
-        return False
-    return True
+
+
+inspector = inspector.inspectorPanel()
+
 
 def main():
     with term.fullscreen(), term.cbreak():
         sys.stdout.write("\033[?25l")
         while True:
             key = term.inkey(timeout=0.01)
+
+            if key:
+                if key.code == term.KEY_UP:
+                    inspector.up()
+                if key.code == term.KEY_DOWN:
+                    inspector.down()
+
             lines = [" "]*(term.height-1)
             lines[0] = "Nodes".center(term.width//2," ")
-            nodes = []
-            indents = []
-            indent = 0
-            newNodeName = False
-            nodeName = ""
-            for i in file.getRaw():
-                if i == "<":
-                    newNodeName = True
-                elif newNodeName and i == "/":
-                    newNodeName = False
-                    indent -= 1
-                elif i =="/":
-                    indent -= 1
-                elif newNodeName and i == ">":
-                    nodes.append(nodeName)
-                    indents.append(indent)
-                    if hasEnd(nodeName):
-                        indent += 1
-                    nodeName = ""
-                    newNodeName = False
-                elif newNodeName:
-                    nodeName += i
-            for i in range(len(nodes)):
-                if indents[i] > 0:
-                    if len(indents) >= i+2 and indents[i+1] == indents[i]:
-                        lines[i+1] = "├─"+nodes[i].split(' ', 1)[0]
-                    else:
-                        lines[i+1] = "└─"+nodes[i].split(' ', 1)[0]
 
+            inspector.parse(file.getRaw())
+
+            for i in range(len(inspector.nodes)):
+                if inspector.nodes[i].indent > 0:
+                    if len(inspector.nodes) >= i+2 and inspector.nodes[i+1].indent == inspector.nodes[i].indent:
+                        lines[i+1] = "├─"+inspector.nodes[i].name.split(' ', 1)[0]
+                    else:
+                        lines[i+1] = "└─"+inspector.nodes[i].name.split(' ', 1)[0]
                 else:
-                    lines[i+1] = nodes[i]
+                    lines[i+1] = inspector.nodes[i].name
 
             for l in range(len(lines)):
                 if len(lines[l]) < term.width//2:
@@ -58,6 +47,7 @@ def main():
                 lines[l] += "│"
 
             lines[0] += "Inspector".center(term.width//2," ")
+
 
             print(term.home + term.clear,end="",flush=True)
             render(lines)
